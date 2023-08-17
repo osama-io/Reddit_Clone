17376e158a30774ddd6b9eec53eb0e472f8603d6
osama <osama.buic@gmail.com>
2023-08-18 00:55:54 +0500

    firebase configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:reddit_clone/features/auth/screens/login_screen.dart';
 import 'package:reddit_clone/theme/palette.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Reddit Clone',
       theme: Palette.lightModeAppTheme,
       home: const Scaffold(
-        body: Placeholder(),
+        body: LoginScreen(),
       ),
     );
   }
